bd0f0525e2afa3616d0d570af41c50dddf1b0dfc
SEBASTIEN FOUCHEUR <21354@cr-bretagne.fr>
2019-12-11 14:48:08 +0100

    Ajout tables

--- models.py
+++ models.py
@@ -11,13 +11,12 @@ class Dataviz(db.Model):
     level = db.Column(db.String(50),nullable=False)
     job = db.Column(db.String(50))
     
-    __table_args__ = {'schema': 'data'}
-    
+    ##__table_args__ = {'schema': 'data'}
     def __repr__(self):
         return '<Dataviz {}>'.format(self.dataviz)
 class Rawdata(db.Model):
     dataviz = db.Column(db.String(50),db.ForeignKey('dataviz.dataviz'),index=True,nullable=False)
-    dataid = db.Column(db.String(50),nullable=False)
+    dataid = db.Column(db.String(50),index=True,nullable=False)
     dataset = db.Column(db.String(50),nullable=False)
     order = db.Column(db.Integer,nullable=False)
     label = db.Column(db.String(250))
@@ -29,3 +28,18 @@ class Rawdata(db.Model):
     def __repr__(self):
         return '<Rawdata {}>'.format(self.dataviz)
 
+class Report(db.Model):
+    report = db.Column(db.String(50),primary_key=True)
+    title = db.Column(db.String(250),nullable=False)
+    def __repr__(self):
+        return '<Report {}>'.format(self.report)
+
+class Report_composition(db.Model):
+    report = db.Column(db.String(50),db.ForeignKey('report.report'),nullable=False)
+    dataviz = db.Column(db.String(50),db.ForeignKey('dataviz.dataviz'),nullable=False)
+    __table_args__ = (
+        PrimaryKeyConstraint('report', 'dataviz'),
+        {},
+    )
+    def __repr__(self):
+        return '<Report_composition {}>'.format(self.report)
