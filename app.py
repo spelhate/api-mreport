bc2bfea0397ee972b28a2ecc33f16180b6035702
sebastien foucheur <sebastien.foucheur@bretagne.bzh>
2019-12-16 13:03:14 +0100

    split functions in another file

--- app.py
+++ app.py
@@ -1,41 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
 from flask_restplus import Api, Resource, fields
-from sqlalchemy import create_engine, bindparam, Integer, String, event, func, inspect, desc
+from sqlalchemy import create_engine, bindparam, Integer, String, event, func,desc
 #from sqlalchemy.schema import PrimaryKeyConstraint
 from sqlalchemy.sql import text
 from flask_sqlalchemy import SQLAlchemy
 import json
 from sqlalchemy.schema import CreateSchema, DropSchema
-from sqlalchemy.exc import NoInspectionAvailable
+from functions_inc import *
 
-## Use for (select *) from a single table ex : {'datavizs': json.loads(json.dumps([row2dict(r) for r in result]))}
-def row2dict(row,label="null"):
-    d = {}
-    try:
-        inspect(row)
-        if(row.__table__.columns):
-            for column in row.__table__.columns:
-                d[column.name] = str(getattr(row, column.name))
-    except NoInspectionAvailable:
-        d[label]=row
-    return d
-## Use for (select atr1,atr2 ...) ex : {'reports': json.loads(json.dumps(dict_builder(result)))}.
-def dict_builder(result):
-    dlist = []
-    for r in result:
-        d={}
-        res = r._asdict()
-        for key in res:
-            d.update(row2dict(res[key],key))
-        dlist.append(d)
-    return dlist
-def insertdb(file,schema):
-    fd = open(file, 'r')
-    sqlFile = fd.read()
-    fd.close()
-    sqlFile = sqlFile.replace("schema.",schema)
-    return sqlFile
 class CherrokeeFix(object):
 
     def __init__(self, app, script_name, scheme):
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 DEBUG = True
 APP_PREFIX = '/api'
 APP_SCHEME = 'http'
-SQLALCHEMY_DATABASE_URI = 'postgresql://<USER>:<PASSWORD>@<HOST>:<PORT>/<DB>'
-##SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+##SQLALCHEMY_DATABASE_URI = 'postgresql://dataviz_admin:graphdata@postsig:5434/dataviz_test'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SCHEMA = "data" ## Comment this line if you don't need schema or if you use SQLite
+##SCHEMA = "test2" ## Comment this line if you don't need schema or if you use SQLite
 
 
 
